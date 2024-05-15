9bc91aa5364bb84106f695ca373c2aa1a1e8cfd8
Andrea <astro11.and@gmail.com>
2024-05-15 15:14:40 +0200

    Updated DatabaseHandler with functions to open and close db and basic error handling

--- DatabaseHandler.h
+++ DatabaseHandler.h
@@ -3,17 +3,23 @@
 
 #include <stdio.h>
 
+#define DATAENTRY_LENGHT sizeof(dataEntry)
+
 typedef struct {
     char name[256];
     char address[256];
     char phoneNumber[256];
 } dataEntry;
 
-void closeFile(FILE *filePointer);
+FILE * open_db_read();
+
+FILE * open_db_write();
+
+void close_db(FILE *filePointer);
 
 void writeEntry(dataEntry dataEntry, FILE *filePointer);
 
-dataEntry readEntry(FILE *filePointer);
+void readEntry(FILE *filePointer, dataEntry * readEntry);
 
 int countEntries(FILE *filePointer, int sizeOfEntry);
 
